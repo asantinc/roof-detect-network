2ebbcc34991dd4961dbff5ac508c8a9c17d0ff5a
asantinc <angiedaniela@gmail.com>
2015-05-27 10:46:57 +0100

    Adding project directories

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Roof detection project',
+    'author': 'Angela Santin',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'angela.santin@outlook.com',
+    'version': '0.1',
+    'install_requires': [],
+    'packages': [],
+    'scripts': [],
+    'name': 'Roof detection'
+}
+
+setup(**config)
---
+++ tests/NAME_tests.py
@@ -0,0 +1,10 @@
+import NAME
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
