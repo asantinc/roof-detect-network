2943e7a5c2f253e603836a3b9f1f3a74468efddb
asantinc <angiedaniela@gmail.com>
2015-06-05 12:12:03 +0100

    Processed patches to feed into basic network

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ except ImportError:
 config = {
     'description': 'Roof detection project',
     'author': 'Angela Santin',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'angela.santin@outlook.com',
+    #'url': 'URL to get it at.',
+    #'download_url': 'Where to download it.',
+    #'author_email': 'angela.santin@outlook.com',
     'version': '0.1',
     'install_requires': [],
     'packages': [],
