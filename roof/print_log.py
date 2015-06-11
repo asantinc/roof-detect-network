947dd8a623eab10d6f360ffe8183dc8b6009fc69
asantinc <angiedaniela@gmail.com>
2015-06-11 12:30:38 +0200

    Removed the colors from the print to file

--- roof/print_log.py
+++ roof/print_log.py
@@ -24,6 +24,38 @@ class PrintLogSave(PrintLog):
         file.write(self.table(nn, train_history))
         file.close()
 
-    def table():
+    def table(self, nn, train_history):
+        info = train_history[-1]
+
+        info_tabulate = OrderedDict([
+            ('epoch', info['epoch']),
+            ('train loss', "{}{:.5f}{}".format(
+                info['train_loss'],
+                )),
+            ('valid loss', "{}{:.5f}{}".format(
+                info['valid_loss'],
+                )),
+            ('train/val', info['train_loss'] / info['valid_loss']),
+            ])
+
+        if not nn.regression:
+            info_tabulate['valid acc'] = info['valid_accuracy']
+
+        if nn.custom_score:
+            info_tabulate[nn.custom_score[0]] = info[nn.custom_score[0]]
+
+        info_tabulate['dur'] = "{:.2f}s".format(info['dur'])
+
+        tabulated = tabulate(
+            [info_tabulate], headers="keys", floatfmt='.5f')
+
+        out = ""
+        if self.first_iteration:
+            out = "\n".join(tabulated.split('\n', 2)[:2])
+            out += "\n"
+            self.first_iteration = False
+
+        out += tabulated.rsplit('\n', 1)[-1]
+        return out
 
 
