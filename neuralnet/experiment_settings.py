9ed5306882c7a42090e1d70e7961b5b14cff069f
asantinc <angiedaniela@gmail.com>
2015-07-07 18:34:22 +0200

    Merged

--- neuralnet/experiment_settings.py
+++ neuralnet/experiment_settings.py
@@ -26,32 +26,7 @@ import skimage
 import load
 import my_net
 import FlipBatchIterator as flip
-=======
-# from collections import OrderedDict
-# from datetime import datetime
-# from functools import reduce
-# import numpy as np
-# import operator
-# import sys
-# import pdb
-# import csv
-# from operator import itemgetter
 
-# import lasagne
-# sys.path.append('~/roof/Lasagne/lasagne')
-# sys.path.append('~/roof/nolearn/nolearn')
-# from nolearn.lasagne.handlers import PrintLog, PrintLayerInfo 
-# from nolearn.lasagne.util import is_conv2d
-# from sklearn.metrics import classification_report, confusion_matrix
-# from scipy.stats import uniform as sp_rand
-# from sklearn.grid_search import RandomizedSearchCV
-# from sklearn.preprocessing import StandardScaler
-
-# #my modules
-# import load
-# import my_net
-# import FlipBatchIterator as flip
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
 
 #Constants for patch production
 PATCHES_OUT_PATH = '/afs/inf.ed.ac.uk/user/s08/s0839470/roof/data/training/'
@@ -101,22 +76,22 @@ def print_debug(to_print, verbosity=1):
         print str(to_print)
 
 
-# class Experiment(object):
-#     def __init__(self, print_out=True, preloaded=False, test_percent=.20, non_roofs=None, 
-#                     roofs_only=False,log=True, plot_loss=True, plot=True, epochs=-1, net_name=None, num_layers=None, max_roofs=None):
-#         self.num_layers = num_layers
-#         self.net_name = net_name
-#         self.epochs = epochs
-#         self.preloaded = preloaded
-#         self.test_percent = test_percent
-#         self.non_roofs = non_roofs    #the proportion of non_roofs relative to roofs to be used in data
-#         self.roofs_only = roofs_only
-#         self.log = log
-#         self.plot_loss = True
-#         self.printer = PrintLogSave()
+class Experiment(object):
+    def __init__(self, print_out=True, preloaded=False, test_percent=.20, non_roofs=None, 
+                    roofs_only=False,log=True, plot_loss=True, plot=True, epochs=-1, net_name=None, num_layers=None, max_roofs=None):
+        self.num_layers = num_layers
+        self.net_name = net_name
+        self.epochs = epochs
+        self.preloaded = preloaded
+        self.test_percent = test_percent
+        self.non_roofs = non_roofs    #the proportion of non_roofs relative to roofs to be used in data
+        self.roofs_only = roofs_only
+        self.log = log
+        self.plot_loss = True
+        self.printer = PrintLogSave()
 
-#         print 'Setting up the Neural Net \n'
-#         self.setup_net(print_out=print_out)
+        print 'Setting up the Neural Net \n'
+        self.setup_net(print_out=print_out)
        
 <<<<<<< HEAD
         print 'Loading data \n'
@@ -146,78 +121,47 @@ def print_debug(to_print, verbosity=1):
             num_layers=self.num_layers,
             input_shape=(None, 3, CROP_SIZE, CROP_SIZE),
             output_num_units=3,
-=======
-#         print 'Loading data \n'
-#         self.roof_loader = load.RoofLoader()
-#         self.X_train, self.X_test, self.y_train, self.y_test = self.roof_loader.load(test_percent=self.test_percent, 
-#                                                 non_roofs=self.non_roofs, roofs_only=self.roofs_only, max_roofs=max_roofs)
-#         print 'Data is loaded \n'
 
-#     def setup_net(self, print_out=True):
-#         if print_out:
-#             on_epoch_finished = [self.printer]
-#             on_training_started = [SaveLayerInfo()]
-#         else:
-#             on_epoch_finished = []
-#             on_training_started = []
-
-#         layers, layer_params = my_net.MyNeuralNet.produce_layers(self.num_layers)      
-#         self.net = my_net.MyNeuralNet(
-#             layers=layers,
-#             num_layers=self.num_layers,
-#             input_shape=(None, 3, CROP_SIZE, CROP_SIZE),
-#             output_num_units=3,
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
+            output_nonlinearity=lasagne.nonlinearities.softmax,
+            preproc_scaler = None, 
             
-#             output_nonlinearity=lasagne.nonlinearities.softmax,
-#             preproc_scaler = None, 
+            #learning rates
+            update_learning_rate=0.01,
+            update_momentum=0.9,
             
-#             #learning rates
-#             update_learning_rate=0.01,
-#             update_momentum=0.9,
-            
-#             #printing
-#             net_name=self.net_name,
-#             on_epoch_finished=on_epoch_finished,
-#             on_training_started=on_training_started,
+            #printing
+            net_name=self.net_name,
+            on_epoch_finished=on_epoch_finished,
+            on_training_started=on_training_started,
 
-#             #data augmentation
-#             batch_iterator_test=flip.CropOnlyBatchIterator(batch_size=128),
-#             batch_iterator_train=flip.FlipBatchIterator(batch_size=128),
+            #data augmentation
+            batch_iterator_test=flip.CropOnlyBatchIterator(batch_size=128),
+            batch_iterator_train=flip.FlipBatchIterator(batch_size=128),
             
-#             max_epochs=self.epochs,
-#             verbose=1,
-#             **layer_params
-#             )
+            max_epochs=self.epochs,
+            verbose=1,
+            **layer_params
+            )
 
 
-#     def train_test(self):
-#         '''
-#         Train and test neural network. Also print out evaluation.
-#         '''
-#         #save settings to file
-#         self.printer.log_to_file(self.net, self.__str__(), overwrite=True)
+    def train_test(self):
+        '''
+        Train and test neural network. Also print out evaluation.
+        '''
+        #save settings to file
+        self.printer.log_to_file(self.net, self.__str__(), overwrite=True)
         
-<<<<<<< HEAD
         #rescale X_train and X_test
         X_test = self.scaler.transform2(self.X_test)
-=======
-#         #rescale X_train and X_test
-#         self.scaler = DataScaler()
-#         X_train = self.scaler.fit_transform(self.X_train)
-#         X_test = self.scaler.transform2(self.X_test)
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
-    
-#         #fit the network to X_train
-#         self.net.fit(X_train, self.y_train)
-#         self.net.save_weights()
 
-#         #find predictions for test set
-#         predicted = self.net.predict(X_test)
-#         self.evaluation(predicted, X_train, X_test, self.y_train, self.y_test)
+        #fit the network to X_train
+        self.net.fit(X_train, self.y_train)
+        self.net.save_weights()
 
+        #find predictions for test set
+        predicted = self.net.predict(X_test)
+        self.evaluation(predicted, X_train, X_test, self.y_train, self.y_test)
 
-<<<<<<< HEAD
 
     def test_image(self, img):
         self.net.load_params_from('saved_weights/conv5_nonroofs1_test20_roofs.pickle')
@@ -245,49 +189,21 @@ def print_debug(to_print, verbosity=1):
         
         #rescale X_train and X_test, using information from the training set only
         X_test = scaler.transform2(self.X_test)
-=======
-#     def test_preloaded_single(self, test_case):
-#         if self.scaler is None:
-#             self.net.load_params_from('saved_weights/'+self.net.net_name+'.pickle')
-             
-#         #rescale X_train and X_test, using information from the training set only
-#         self.scaler = DataScaler()
-#         self.scaler.fit_transform(self.X_train)
-#         test_case = self.scaler.trasform(test_case)
-#         return self.net.predict(self.X_test)
+        #find predictions for test set
+        self.net.load_params_from('saved_weights/'+self.net.net_name+'.pickle')
+        predicted = self.net.predict(X_test)
+        self.evaluation(predicted, X_train, X_test, self.y_train, self.y_test)
 
 
-#     def test_preloaded(self, plot_loss=True, test_case=None):  
-#         '''Preload weights, classify roofs and write evaluation
-#         To classify a single instance it must be passed in as the test_case parameter. Otherwise,
-#         the method will test the network on the test portion of the training set
-#         '''
-#         #save settings to file
-#         self.printer.log_to_file(self.net, self.__str__(), overwrite=True)
-#         self.roof_loader = load.RoofLoader()
+    def evaluation(self, predicted, X_train, X_test, y_train, y_test):
+        #print evaluation
+        self.printer.log_to_file(self.net, confusion_matrix(y_test, predicted), binary=True, title='\n\nConfusion Matrix\n')
+        self.printer.log_to_file(self.net, classification_report(y_test, predicted), title='\n\nReport\n')
         
-#         #rescale X_train and X_test, using information from the training set only
-#         self.scaler = DataScaler()
-#         X_train = scaler.fit_transform(self.X_train)
-#         X_test = scaler.transform2(self.X_test)
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
-
-#         #find predictions for test set
-#         self.net.load_params_from('saved_weights/'+self.net.net_name+'.pickle')
-#         predicted = self.net.predict(X_test)
-#         self.evaluation(predicted, X_train, X_test, self.y_train, self.y_test)
-
-
-#     def evaluation(self, predicted, X_train, X_test, y_train, y_test):
-#         #print evaluation
-#         self.printer.log_to_file(self.net, confusion_matrix(y_test, predicted), binary=True, title='\n\nConfusion Matrix\n')
-#         self.printer.log_to_file(self.net, classification_report(y_test, predicted), title='\n\nReport\n')
+        #save a plot of the validation and training losses
+        #if self.plot_loss:
+        self.net.save_loss()
         
-#         #save a plot of the validation and training losses
-#         #if self.plot_loss:
-#         self.net.save_loss()
-        
-<<<<<<< HEAD
         #display mistakes
         if self.display_mistakes: 
             mistakes = np.array([True if y_test[i]-predicted[i] != 0 else False for i in range(len(y_test))])
@@ -354,151 +270,82 @@ class PrintLogSave(PrintLog):
     def table(self, nn, train_history):
         info = train_history[-1]
         return str(info['epoch'])+'\t'+ str(info['train_loss'])+ '\t'+str(info['valid_loss'])+'\t'+str( info['train_loss'] / info['valid_loss'])+'\t'+str(info['valid_accuracy'])+'\n'
-=======
-#         #display mistakes
-#         if self.display_mistakes: 
-#             mistakes = np.array([True if y_test[i]-predicted[i] != 0 else False for i in range(len(y_test))])
-#             mistaken_imgs = X_test[mistakes]
-#             mistaken_imgs = scaler.inverse_transform(mistaken_imgs)
-#             roof_loader.display_images(mistaken_imgs, labels=y_test[mistakes], indeces=range(len(mistaken_imgs)))
-
-
-#     @staticmethod
-#     def report_grid(grid_scores, n_top=3):
-#         top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
-#         for i , score in enumerate(top_scores):
-#             print "Model with rank: {0}".format(i+1)
-#             print "Mean validation score: {0:.3f} (std: {1:.3f})".format(
-#                     score.mean_validation_score,
-#                     np.std(score.cv_validation_scores))
-#             print "Parameters: {0}".format(score.parameters)
-#             print ""
-
-#     def optimize_params(self):
-#         params_grid = {"update_learning_rate": sp_rand(0.001,0.01)}
-#         rsearch = RandomizedSearchCV(estimator=self.net, param_distributions=params_grid, n_iter=15, n_jobs=1)
-#         X, _, y, _ = self.roof_loader.load(max_roofs=100, test_percent=0, non_roofs=self.non_roofs, roofs_only=self.roofs_only)
-#         rsearch.fit(X,y)
-#         Experiment.report_grid(rsearch.grid_scores_)
-
-
-#     def __str__(self):
-#         out_list = list()
-#         for key, value in self.__dict__.items():
-#             out_list.append(str(key)+': '+str(value))
-#         out_list.append('\n')
-#         return '\n'.join(out_list)          
-
-
-# class DataScaler(StandardScaler):
-#     #Subclass of sklearn.StandardScaler that reshapes data as needed and then calls super to do scaling
-#     #
-#     def fit_transform(self, X):
-#         X_shape = X.shape
-#         X = X.reshape(X_shape[0], X_shape[1]*X_shape[2]*X_shape[3])
-#         X = super(DataScaler, self).fit_transform(X)
-#         return X.reshape(X_shape[0], X_shape[1], X_shape[2], X_shape[3])
-
-#     def transform2(self, X):
-#         X_shape = X.shape
-#         X = X.reshape(X_shape[0], X_shape[1]*X_shape[2]*X_shape[3])
-#         X = super(DataScaler, self).transform(X)
-#         return X.reshape(X_shape[0], X_shape[1], X_shape[2], X_shape[3])
-
-#     def inverse_transform(self, X): 
-#         X_shape = X.shape
-#         X = X.reshape(X_shape[0], X_shape[1]*X_shape[2]*X_shape[3])
-#         X = super(DataScaler, self).inverse_transform(X)
-#         return X.reshape(X_shape[0], X_shape[1], X_shape[2], X_shape[3])
-
-
-# class PrintLogSave(PrintLog):
-#     def __call__(self, nn, train_history): 
-#         file = open(OUT_HISTORY+nn.net_name, 'a')
-#         file.write(self.table(nn, train_history))
-#         file.close()
-
-#     def table(self, nn, train_history):
-#         info = train_history[-1]
-#         return str(info['epoch'])+'\t'+ str(info['train_loss'])+ '\t'+str(info['valid_loss'])+'\t'+str( info['train_loss'] / info['valid_loss'])+'\t'+str(info['valid_accuracy'])+'\n'
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
             
-#     def log_to_file(self, nn, log, overwrite=False, binary=False, title=''):
-#         write_type = 'w' if overwrite else 'a'
-#         file = open(OUT_REPORT+nn.net_name, write_type)
-#         file.write(title)
-#         if binary:
-#             print >> file, log
-#         else:
-#             file.write(log)
-#         file.close()
+    def log_to_file(self, nn, log, overwrite=False, binary=False, title=''):
+        write_type = 'w' if overwrite else 'a'
+        file = open(OUT_REPORT+nn.net_name, write_type)
+        file.write(title)
+        if binary:
+            print >> file, log
+        else:
+            file.write(log)
+        file.close()
 
 
-# class SaveLayerInfo(PrintLayerInfo):
-#     def __call__(self, nn, train_history):
-#         file = open(OUT_REPORT+nn.net_name, 'a')
-#         message = self._get_greeting(nn)
-#         file.write(message)
-#         file.write("## Layer information")
-#         file.write("\n\n")
+class SaveLayerInfo(PrintLayerInfo):
+    def __call__(self, nn, train_history):
+        file = open(OUT_REPORT+nn.net_name, 'a')
+        message = self._get_greeting(nn)
+        file.write(message)
+        file.write("## Layer information")
+        file.write("\n\n")
 
-#         layers_contain_conv2d = is_conv2d(list(nn.layers_.values()))
-#         if not layers_contain_conv2d or (nn.verbose < 2):
-#             layer_info = self._get_layer_info_plain(nn)
-#             legend = None
-#         else:
-#             layer_info, legend = self._get_layer_info_conv(nn)
-#         file.write(layer_info)
-#         if legend is not None:
-#             file.write(legend)
-#         file.write(" \n\n")
+        layers_contain_conv2d = is_conv2d(list(nn.layers_.values()))
+        if not layers_contain_conv2d or (nn.verbose < 2):
+            layer_info = self._get_layer_info_plain(nn)
+            legend = None
+        else:
+            layer_info, legend = self._get_layer_info_conv(nn)
+        file.write(layer_info)
+        if legend is not None:
+            file.write(legend)
+        file.write(" \n\n")
 
-#         file.close()
-
-
-# def set_parameters():
-#     try:
-#         opts, args = getopt.getopt(sys.argv[1:], "t:n:l:p:r:a:e:")
-#     except getopt.GetoptError:
-#         print 'Command line error'
-#         sys.exit(2) 
-#     test_percent=0.2
-#     non_roofs=1
-#     preloaded=False
-#     num_layers=0 #logistic
-#     roofs_only=True
-#     plot=True
-#     net_name=None
-#     epochs=250
-#     for opt, arg in opts:
-#         if opt == '-t':
-#             test_percent=float(arg)
-#         elif opt == '-n':
-#             num_layers = int(float(arg))
-#         elif opt=='-a':
-#             net_name=arg
-#         elif opt=='-e':
-#             epochs=int(float(arg))
-#     return test_percent, non_roofs, preloaded, num_layers, roofs_only, plot, net_name, epochs
+        file.close()
 
 
-# def get_params_from_file(file_name):
-#     parameters = dict()
-#     with open(file_name, 'r') as f:
-#         reader = csv.reader(f, delimiter=',')
-#         for par in reader:
-#             if len(par) == 2: 
-#                 if par[0].strip() == 'test_percent':
-#                     parameters[par[0].strip()] = (float(par[1].strip()))
-#                 else:
-#                     parameters[par[0].strip()] = int(float(par[1].strip()))
-#     return parameters
+def set_parameters():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "t:n:l:p:r:a:e:")
+    except getopt.GetoptError:
+        print 'Command line error'
+        sys.exit(2) 
+    test_percent=0.2
+    non_roofs=1
+    preloaded=False
+    num_layers=0 #logistic
+    roofs_only=True
+    plot=True
+    net_name=None
+    epochs=250
+    for opt, arg in opts:
+        if opt == '-t':
+            test_percent=float(arg)
+        elif opt == '-n':
+            num_layers = int(float(arg))
+        elif opt=='-a':
+            net_name=arg
+        elif opt=='-e':
+            epochs=int(float(arg))
+    return test_percent, non_roofs, preloaded, num_layers, roofs_only, plot, net_name, epochs
 
-# if __name__ == '__main__':
-#     #test_percent, non_roofs, preloaded, num_layers, roofs_only, plot, net_name, epochs = set_parameters()  
-#     params = get_params_from_file(NET_PARAMS_PATH+raw_input('Parameter file NAME: '))
+
+def get_params_from_file(file_name):
+    parameters = dict()
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        for par in reader:
+            if len(par) == 2: 
+                if par[0].strip() == 'test_percent':
+                    parameters[par[0].strip()] = (float(par[1].strip()))
+                else:
+                    parameters[par[0].strip()] = int(float(par[1].strip()))
+    return parameters
+
+if __name__ == '__main__':
+    #test_percent, non_roofs, preloaded, num_layers, roofs_only, plot, net_name, epochs = set_parameters()  
+    params = get_params_from_file(NET_PARAMS_PATH+raw_input('Parameter file NAME: '))
     
-<<<<<<< HEAD
     if params['net_name'] == 0:
         params['net_name'] = 'conv{0}_nonroofs{1}_test{2}'.format(params['num_layers'], params['non_roofs'], 100*params['test_percent']) 
     if params['roofs_only']:
@@ -515,16 +362,4 @@ class PrintLogSave(PrintLog):
     else:
         img = cv2.imread("../data/inhabited/0001.jpg")
         experiment.test_image(img)
-=======
-#     if params['net_name'] == 0:
-#         params['net_name'] = 'conv{0}_nonroofs{1}_test{2}'.format(params['num_layers'], params['non_roofs'], 100*params['test_percent']) 
-#     if params['roofs_only']:
-#         params['net_name'] = params['net_name']+'_roofs'
-#     print 'Network name is: {0}'.format(params['net_name'])
 
-#     #set up the experiment
-#     experiment = Experiment(print_out= False, **params) 
-#     experiment.optimize_params()
-#     #experiment.train_test() 
-
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
--- neuralnet/get_data.py
+++ neuralnet/get_data.py
@@ -516,31 +516,27 @@ class DataLoader(object):
                 # for r, roof in enumerate(roof_list):
                 #     self.produce_roof_patches(img_path=img_path, img_id=i, roof=roof)
 
-<<<<<<< HEAD
-    with open(settings.LABELS_PATH, 'w') as label_file:
-        max_w = 0
-        max_h = 0
-        for i, img in enumerate(img_names):
-#            print 'Processing image: '+str(i)+'\n'
-            img_path = settings.INHABITED_PATH+img
-            xml_path = settings.INHABITED_PATH+img[:-3]+'xml'
+#     with open(settings.LABELS_PATH, 'w') as label_file:
+#         max_w = 0
+#         max_h = 0
+#         for i, img in enumerate(img_names):
+# #            print 'Processing image: '+str(i)+'\n'
+#             img_path = settings.INHABITED_PATH+img
+#             xml_path = settings.INHABITED_PATH+img[:-3]+'xml'
 
-            roof_list, cur_max_w, cur_max_h = loader.get_roofs(xml_path)
-           # max_h = cur_max_h if (max_h<cur_max_h) else max_h
-           # max_w = cur_max_w if (max_w<cur_max_h) else max_h
+#             roof_list, cur_max_w, cur_max_h = loader.get_roofs(xml_path)
+#            # max_h = cur_max_h if (max_h<cur_max_h) else max_h
+#            # max_w = cur_max_w if (max_w<cur_max_h) else max_h
 
-            for r, roof in enumerate(roof_list):
-#                print 'Processing roof: '+str(r)+'\n'
-                loader.produce_roof_patches(img_path=img_path, img_id=i+1, 
-                                    roof=roof, label_file=label_file, max_h=max_h, max_w=max_w)
-        neg_patches_wanted = settings.NEGATIVE_PATCHES_NUM*loader.total_patch_no
-        loader.get_negative_patches(neg_patches_wanted, label_file)
-        #settings.print_debug('************* Total patches saved: *****************: '+str(loader.total_patch_no))
-=======
-'''
+#             for r, roof in enumerate(roof_list):
+# #                print 'Processing roof: '+str(r)+'\n'
+#                 loader.produce_roof_patches(img_path=img_path, img_id=i+1, 
+#                                     roof=roof, label_file=label_file, max_h=max_h, max_w=max_w)
+#         neg_patches_wanted = settings.NEGATIVE_PATCHES_NUM*loader.total_patch_no
+#         loader.get_negative_patches(neg_patches_wanted, label_file)
+#         #settings.print_debug('************* Total patches saved: *****************: '+str(loader.total_patch_no))
+
 if __name__ == '__main__':
     loader = DataLoader(labels_path='labels.csv', out_path='../data/testing_json/', in_path=settings.JSON_IMGS)
     loader.produce_json_roofs(json_file='../data/images-new/labels.json')
-'''
->>>>>>> facd4c0a807554d3ce5d6f66adc00ae28a194d39
 
