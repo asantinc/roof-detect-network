Fixing the rotation problem of the original roofs

neuralnet/get_data.py
@@ -324,9 +324,12 @@ class DataLoader(object):
 
 
 
-    def get_roof_patches_from_rectified_dataset(self, xml_path=utils.RECTIFIED_COORDINATES, xml_name=None, img_path=None):
+    def get_roof_patches_from_rectified_dataset(self, coordinates_only=False, xml_path=utils.RECTIFIED_COORDINATES, xml_name=None, img_path=None):
+        '''
+        Return roof patches from the dataset that has roofs properly bounded
+        If coordinated_only is True, return only the coordinates instead of the patches
+        '''
         assert xml_name is not None
-        assert img_path is not None
         xml_path = xml_path+xml_name
 
         #EXTRACT THE POLYGONS FROM THE XML
@@ -354,18 +357,22 @@ class DataLoader(object):
                         if len(polygon) == 4:
                             polygon_list.append(polygon)
 
-        #EXTRACT THE RECTIFIED ROOF PATCH FROM THE IMG, RETURN THE ROOF PATCHES
-        try:
-            img = cv2.imread(img_path, flags=cv2.IMREAD_COLOR)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            gray_equalized = cv2.equalizeHist(gray)
-        except IOError as e:
-            print e
-            sys.exit(-1)
-        roof_patches = list()
-        for i, polygon in enumerate(polygon_list):
-			roof_patches.append(four_point_transform(gray_equalized, np.array(polygon, dtype = "float32")))
-        return roof_patches 
+        if coordinates_only:
+            return polygon_list
+        else:
+            #EXTRACT THE RECTIFIED ROOF PATCH FROM THE IMG, RETURN THE ROOF PATCHES
+            assert img_path is not None
+            try:
+                img = cv2.imread(img_path, flags=cv2.IMREAD_COLOR)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                gray_equalized = cv2.equalizeHist(gray)
+            except IOError as e:
+                print e
+                sys.exit(-1)
+            roof_patches = list()
+            for i, polygon in enumerate(polygon_list):
+                roof_patches.append(four_point_transform(gray_equalized, np.array(polygon, dtype = "float32")))
+            return roof_patches 
 
 
 

neuralnet/reporting.py
@@ -60,6 +60,11 @@ class Evaluation(object):
         for img_name in self.img_names:
             self.roofs[img_name] = DataLoader().get_roofs(self.in_path+img_name[:-3]+'xml', img_name)
 
+        self.correct_roofs = dict()
+        self.correct_roofs_angled = dict()
+        for img_name in self.img_names:
+            self.correct_roofs[img_name] = DataLoader().get_roof_patches_from_rectified_dataset(coordinates_only=True, xml_name=img_name[:-3]+'xml')
+
         self.out_path = out_path
         if detector_names is not None:
             self.init_report(detector_names)
@@ -124,7 +129,6 @@ class Evaluation(object):
         detections = {'metal': self.detections.get_img_detections_specific_type('metal', img_name),
                         'thatch': self.detections.get_img_detections_specific_type('thatch', img_name)}
 
-        print false_pos
         for roof in self.roofs[img_name]:
             best_voc_score = -1
             roof_area = roof.width*roof.height
@@ -176,10 +180,10 @@ class Evaluation(object):
         if self.save_imgs:
             img = None
             try:
-                img = self.mark_roofs_on_img(img_name = img_name, roofs=self.false_positive_coords[img_name], color=(0,0,0))
+                img = self.mark_roofs_on_img(img_name = img_name, roofs=self.false_positive_coords[img_name], color=(0,0,255))
             except KeyError:
                 pass
-            img = self.mark_roofs_on_img(img_name=img_name, img=img, roofs=self.roofs[img_name], color=(0,255,0))
+            img = self.mark_roofs_on_img(img_name=img_name, img=img, roofs=self.roofs[img_name], color=(0,0,0))
             temp_img = None
             try:
                 temp_img = self.mark_roofs_on_img(img_name=img_name, img=img, roofs=self.true_positive_coords[img_name], color=(0,255,0))
@@ -309,5 +313,108 @@ class Evaluation(object):
         img = self.mark_roofs_on_img(img, img_name, roof_list)
         cv2.imwrite(self.out_path+'_'+img_name, img)
 
+
+    def score_img_rectified(self, img_name, rotated_images): #rows=1200, cols=2000):
+        '''Find best overlap between each roof in an img and the detections,
+        according the VOC score
+        '''
+        voc_scores = list()
+        #start with all detections as false pos; as we find matches, we increase the true_pos, decrese the false_pos numbers
+        false_pos = {'metal': len(self.detections.get_img_detections_specific_type('metal', img_name)), 
+                            'thatch': len(self.detections.get_img_detections_specific_type('thatch', img_name))}
+        true_pos = defaultdict(int) 
+        detections = {'metal': self.detections.get_img_detections_specific_type('metal', img_name),
+                        'thatch': self.detections.get_img_detections_specific_type('thatch', img_name)}
+
+                    
+        for roof in self.correct_roofs[img_name]:
+            best_voc_score = -1
+            #roof_type = roof.roof_type
+            #TODO: currently only metal is supported...
+            roof_type = 'metal'
+
+            if roof_type == 'thatch':
+                #TODO: need to fix this
+                print 'SKIPPING THACHT'
+                continue
+
+            for angle in detections[roof_type].keys():
+                img_rotated = rotated_images[angle]
+                angled_roof = list()
+                for point in roof:
+                    #TODO need to pass in the size of the rotated image to do a proper rotation
+                    angled = utils.rotate_point(point, img_rotated, angle)
+                    angled_roof.append(angled)
+
+                try:
+                    x_true, y_true, w_true, h_true = -1, -1, -1, -1                
+                    for (x,y,w,h) in detections[roof_type][angle]:                           #for each patch found
+                        matching_mask = np.zeros(img_rotated.shape[:2], dtype='uint8')
+                        cv2.fillConvexPoly(matching_mask, np.array(angled_roof), 1)
+                        roof_area = Evaluation.sum_mask(matching_mask)
+
+                        matching_mask[y:y+h, x:x+w] = 0        #detection
+                        roof_missed = Evaluation.sum_mask(matching_mask)
+                        
+                        intersection_area = roof_area-roof_missed
+                        detection_area = w*h
+                        union_area = (roof_area+(detection_area) )-intersection_area
+                        voc_score = float(intersection_area)/union_area
+
+                        if voc_score > best_voc_score:
+                            if x_true != -1:#put prev detection in false pos: score was lower and  we can only detect a roof once
+                                self.false_positive_coords[img_name].append((x_true, y_true, w_true, h_true))
+                            best_voc_score = voc_score
+                            #rotate back so we can display it properly in the non-rotated image
+                            y_true, x_true = utils.rotate_point((y, x), img_rotated, -angle)
+                            w_true, h_true = w, h
+
+                        else:
+                            y, x = utils.rotate_point((y,x), img_rotated, -angle)
+                            self.false_positive_coords[img_name].append((x, y, w, h))
+                            
+                    voc_scores.append(best_voc_score)
+                except ValueError as e:
+                    print e
+
+            if (best_voc_score >= self.VOC_threshold):
+                true_pos[roof_type] += 1
+                false_pos[roof_type] -= 1
+                self.true_positive_coords[img_name].append((x_true, y_true, w_true, h_true))
+
+        for roof_type in self.roof_types:
+            false_pos[roof_type] = len(detections[roof_type])-true_pos[roof_type] 
+            print '*****'+roof_type+'*****'
+            roofs_num = len([roof for roof in self.roofs[img_name] if roof.roof_type == roof_type])
+            print roofs_num
+            print 'True pos: {0}'.format(true_pos[roof_type])
+            print 'False pos: {0}'.format(false_pos[roof_type])
+            self.scores['total_true_pos'][roof_type]+= true_pos[roof_type]
+            self.scores['total_false_pos'][roof_type] += false_pos[roof_type]
+            self.scores['total_detections'][roof_type] += len(self.detections.get_img_detections_specific_type(roof_type, img_name))
+            self.scores['total_roofs'][roof_type] += roofs_num 
+
+        self.save_images(img_name)
+
+
+
+    def save_images(self, img_name):
+        '''Displays the ground truth, along with the true and false positives for a given image
+        '''
+        if self.save_imgs:
+            img = None
+            try:
+                img = self.mark_roofs_on_img(img_name = img_name, roofs=self.false_positive_coords[img_name], color=(0,0,255))
+            except KeyError:
+                pass
+            img = self.mark_roofs_on_img(img_name=img_name, img=img, roofs=self.roofs[img_name], color=(0,0,0))
+            temp_img = None
+            try:
+                temp_img = self.mark_roofs_on_img(img_name=img_name, img=img, roofs=self.true_positive_coords[img_name], color=(0,255,0))
+            except KeyError:
+                pass
+            if temp_img is not None:
+                img = temp_img
+            cv2.imwrite(self.out_path+img_name[:-4]+'TP_FP_.jpg', img)
 
 

neuralnet/utils.py
@@ -6,7 +6,10 @@ import sys
 import pdb
 import datetime
 import csv
-
+import math
+import cv2
+import numpy as np
+import warnings
 
 ###################################
 #types of roof
@@ -26,6 +29,8 @@ SMALL_TEST = 4
 
 IN = 1  #input 
 OUT = 2 #output
+
+VIOLA_ANGLES = [0, 45, 90, 135]
 
 ###################################
 #VIOLA TRAINING
@@ -166,7 +171,10 @@ def get_path(in_or_out=None, out_folder_name=None, params=False, full_dataset=Fa
         else:
             raise ValueError('Must specify input or output folder.')
         if out_folder_name is not None:
-            check_append(path, out_folder_name)
+            if out_folder_name.endswith('/'):
+                check_append(path, out_folder_name)
+            else:
+                check_append(path, out_folder_name+'/')
     return ''.join(path)
      
 
@@ -251,6 +259,7 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 
 
 def rotate_point(pos, img, theta):
+    warnings.warn('rotate_point assumes image is size 1200,2000, but this may not always be the case')
     theta = math.radians(theta)
 
     #translation: how much the image has moved
@@ -264,7 +273,6 @@ def rotate_point(pos, img, theta):
     #rotate the point
     p_x = math.cos(theta) * (px-ox) - math.sin(theta) * (py-oy) + ox
     p_y = math.sin(theta) * (px-ox) + math.cos(theta) * (py-oy) + oy
-    print p_y, p_x
     return int(p_y), int(p_x)
 
 
@@ -285,13 +293,89 @@ def rotate_image(image, angle):
   diagonal = int(math.sqrt(pow(image.shape[0], 2) + pow(image.shape[1], 2)))
   offset_x = (diagonal - image.shape[0])/2
   offset_y = (diagonal - image.shape[1])/2
+  #dst_image = np.zeros((diagonal, diagonal, 3), dtype='uint8')
+  dst_image = np.zeros((diagonal, diagonal), dtype='uint8')
+  image_center = (diagonal/2, diagonal/2)
+
+  R = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  #dst_image[offset_x:(offset_x + image.shape[0]), \
+  #          offset_y:(offset_y + image.shape[1]), \
+  #          :] = image
+  dst_image[offset_x:(offset_x + image.shape[0]), offset_y:(offset_y + image.shape[1])] = image
+  dst_image = cv2.warpAffine(dst_image, R, (diagonal, diagonal), flags=cv2.INTER_LINEAR)
+  # Calculate the rotated bounding rect
+  x0 = offset_x
+  x1 = offset_x + image.shape[0]
+  x2 = offset_x
+  x3 = offset_x + image.shape[0]
+
+  y0 = offset_y
+  y1 = offset_y
+  y2 = offset_y + image.shape[1]
+  y3 = offset_y + image.shape[1]
+
+  corners = np.zeros((3,4))
+  corners[0,0] = x0
+  corners[0,1] = x1
+  corners[0,2] = x2
+  corners[0,3] = x3
+  corners[1,0] = y0
+  corners[1,1] = y1
+  corners[1,2] = y2
+  corners[1,3] = y3
+  corners[2:] = 1
+
+  c = np.dot(R, corners)
+
+  x = int(c[0,0])
+  y = int(c[1,0])
+  left = x
+  right = x
+  up = y
+  down = y
+
+  for i in range(4):
+    x = int(c[0,i])
+    y = int(c[1,i])
+    if (x < left): left = x
+    if (x > right): right = x
+    if (y < up): up = y
+    if (y > down): down = y
+  h = down - up
+  w = right - left
+
+  #cropped = np.zeros((w, h, 3), dtype='uint8')
+  cropped = np.zeros((w, h), dtype='uint8')
+  #cropped[:, :, :] = dst_image[left:(left+w), up:(up+h), :]
+  cropped[:, :] = dst_image[left:(left+w), up:(up+h)]
+  return cropped
+
+def rotate_image_RGB(image, angle):
+  '''Rotate image "angle" degrees.
+
+  How it works:
+    - Creates a blank image that fits any rotation of the image. To achieve
+      this, set the height and width to be the image's diagonal.
+    - Copy the original image to the center of this blank image
+    - Rotate using warpAffine, using the newly created image's center
+      (the enlarged blank image center)
+    - Translate the four corners of the source image in the enlarged image
+      using homogenous multiplication of the rotation matrix.
+    - Crop the image according to these transformed corners
+  '''
+
+  diagonal = int(math.sqrt(pow(image.shape[0], 2) + pow(image.shape[1], 2)))
+  offset_x = (diagonal - image.shape[0])/2
+  offset_y = (diagonal - image.shape[1])/2
   dst_image = np.zeros((diagonal, diagonal, 3), dtype='uint8')
+  #dst_image = np.zeros((diagonal, diagonal), dtype='uint8')
   image_center = (diagonal/2, diagonal/2)
 
   R = cv2.getRotationMatrix2D(image_center, angle, 1.0)
   dst_image[offset_x:(offset_x + image.shape[0]), \
             offset_y:(offset_y + image.shape[1]), \
             :] = image
+  #dst_image[offset_x:(offset_x + image.shape[0]), offset_y:(offset_y + image.shape[1])] = image
   dst_image = cv2.warpAffine(dst_image, R, (diagonal, diagonal), flags=cv2.INTER_LINEAR)
   # Calculate the rotated bounding rect
   x0 = offset_x
@@ -335,8 +419,11 @@ def rotate_image(image, angle):
   w = right - left
 
   cropped = np.zeros((w, h, 3), dtype='uint8')
+  #cropped = np.zeros((w, h), dtype='uint8')
   cropped[:, :, :] = dst_image[left:(left+w), up:(up+h), :]
+  #cropped[:, :] = dst_image[left:(left+w), up:(up+h)]
   return cropped
+
 
 
 if __name__ == '__main__':

neuralnet/viola_detector.py
@@ -18,37 +18,39 @@ from timer import Timer
 
 from reporting import Evaluation, Detections
 
-            
+DEBUG = True
+
 class ViolaDetector(object):
     def __init__(self, 
             in_path=None, 
-            detector_names=None, 
             out_path=None,
-            out_folder_name=None,
+            folder_name=None,
             save_imgs=False,
-            scale=1.05,
-            old_detector=True,
+            detector_names=None, 
+            group=None, #minNeighbors, scale...
+            min_neighbors=3,
+            scale=1.1,
+            rotate=False 
             ):
-        assert out_folder_name is not None
         assert in_path is not None
+        self.in_path = in_path
         assert out_path is not None
+        self.out_folder = out_path
+        print 'Will output evaluation to: {0}'.format(self.out_folder)
 
         self.img_names = [f for f in os.listdir(in_path) if f.endswith('.jpg')]
-        self.scale = scale
         self.save_imgs = save_imgs
+
+        #parameters for detection 
+        self.scale = scale
+        self.min_neighbors = min_neighbors
+        self.group = group
+        self.angles = utils.VIOLA_ANGLES if rotate else [0]
 
         self.viola_detections = Detections()
         self.setup_detectors(detector_names)
 
-        self.in_path = in_path
-
-        assert out_folder_name is not None
-        #self.out_folder = out_path+out_folder_name
-        self.out_folder = out_path
-        utils.mkdir(self.out_folder)
-        print 'Will output evaluation to: {0}'.format(self.out_folder)
-
-        self.evaluation = Evaluation(method='viola', folder_name=out_folder_name, out_path=self.out_folder, detections=self.viola_detections, 
+        self.evaluation = Evaluation(method='viola', folder_name=folder_name, out_path=self.out_folder, detections=self.viola_detections, 
                     in_path=self.in_path, detector_names=detector_names)
 
 
@@ -60,32 +62,34 @@ class ViolaDetector(object):
         self.roof_detectors = defaultdict(list)
         self.detector_names = detector_names
 
-        if old_detector:
-            self.roof_detectors['metal'] = [cv2.CascadeClassifier('../viola_jones/cascades/'+path) for path in detector_names['metal']]
-            self.roof_detectors['thatch'] = [cv2.CascadeClassifier('../viola_jones/cascades/'+path) for path in detector_names['thatch']]
+        #if old_detector:
+        #    self.roof_detectors['metal'] = [cv2.CascadeClassifier('../viola_jones/cascades/'+path) for path in detector_names['metal']]
+        #    self.roof_detectors['thatch'] = [cv2.CascadeClassifier('../viola_jones/cascades/'+path) for path in detector_names['thatch']]
 
-        else:
-            for roof_type in ['metal', 'thatch']:
-                for path in detector_names[roof_type]: 
-                    if path.startswith('cascade'):
-                        start = '../viola_jones/rectified/' if '_rect_' in path else '../viola_jones/'
-                        self.roof_detectors[roof_type].append(cv2.CascadeClassifier(start+path+'/cascade.xml'))
-                    else:
-                        self.roof_detectors[roof_type].append(cv2.CascadeClassifier('../viola_jones/cascade_'+path+'/cascade.xml'))
+        #else:
+        for roof_type in ['metal', 'thatch']:
+            for path in detector_names[roof_type]: 
+                if path.startswith('cascade'):
+                    start = '../viola_jones/rectified/' if '_rect_' in path else '../viola_jones/'
+                    self.roof_detectors[roof_type].append(cv2.CascadeClassifier(start+path+'/cascade.xml'))
+                else:
+                    self.roof_detectors[roof_type].append(cv2.CascadeClassifier('../viola_jones/cascade_'+path+'/cascade.xml'))
 
 
-    def detect_roofs_in_img_folder(self, group=None, reject_levels=1.3, level_weights=5, scale=1.05):
+    def detect_roofs_in_img_folder(self):
         '''Compare detections to ground truth roofs for set of images in a folder
         '''
         for i, img_name in enumerate(self.img_names):
             print 'Processing image {0}/{1}\t{2}'.format(i, len(self.img_names), img_name)
-            self.detect_roofs(group=group, img_name=img_name, reject_levels=reject_levels, level_weights=level_weights, scale=scale)
-            self.evaluation.score_img(img_name)
+            _, _, rotated_images = self.detect_roofs(img_name)
+            self.evaluation.score_img_rectified(img_name, rotated_images)
         self.evaluation.print_report()
         open(self.out_folder+'DONE', 'w').close() 
 
 
-    def detect_roofs(self, img_name=None, group=None, reject_levels=1.3, level_weights=5, scale=None, min_neighbors=1, eps=2):
+#, img_name=None, group=None, reject_levels=1.3, level_weights=5, scale=None, min_neighbors=1, eps=2
+    def detect_roofs(self, img_name):
+        rotated_images = dict()
         try:
             img = cv2.imread(self.in_path+img_name, flags=cv2.IMREAD_COLOR)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -94,30 +98,74 @@ class ViolaDetector(object):
             print e
             sys.exit(-1)
         else:
-            self.scale = scale if scale is not None else self.scale
-
-            #get detections in the image
             self.total_detection_time = 0
             detected_roofs = defaultdict(list)
+
             for roof_type in self.roof_detectors.keys():
+                detected_roofs[roof_type] = defaultdict(list)
                 for i, detector in enumerate(self.roof_detectors[roof_type]):
-                    print 'Detecting with detector: '+str(i)
-                    checking =  detector.empty()
-                    print roof_type, checking
-                    with Timer() as t: 
-                        detected_roofs[roof_type].extend(detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=3))
-                    print 'Time: {0}'.format(t.secs)
-                    self.viola_detections.total_time += t.secs
-                    #group_detected_roofs, weights = cv2.groupRectangles(np.array(detected_roofs).tolist(), 0, 5)
+
+                    #ANGLE ROTATIONS AND DETECTIONS
+                    for angle in self.angles:
+                        #separate detections by angle
+                        #detected_roofs[roof_type][angle] = defaultdict(list)
+                        print 'Detecting with detector: '+str(i)
+                        print 'ANGLE '+str(angle)
+                        with Timer() as t: 
+
+                            if angle == 0:
+                                temp_detections = detector.detectMultiScale(gray, scaleFactor=self.scale, minNeighbors=self.min_neighbors )
+                                final_detections = temp_detections
+                                rotated_images[angle] = gray 
+                                
+                            else:
+                                rotated_img = utils.rotate_image(gray, angle)
+                                rotated_images[angle] = rotated_img
+                                temp_detections =  detector.detectMultiScale(rotated_img, scaleFactor=self.scale, minNeighbors=self.min_neighbors)
+                                final_detections = self.transform_points(temp_detections, rotated_img, angle)
+
+                            detected_roofs[roof_type][angle].extend(final_detections)
+
+                        print 'Time detection: {0}'.format(t.secs)
+                        self.viola_detections.total_time += t.secs
+
+                        if DEBUG:
+                            rgb_rotated = utils.rotate_image_RGB(img, angle)
+                            self.mark_save_current_rotation(img_name, rgb_rotated, temp_detections, angle)
+
                     print 'DETECTED {0} roofs'.format(len(detected_roofs[roof_type]))
-                if group == 'group_rectangles':
-                    detected_roofs[roof_type], weights = cv2.groupRectangles(np.array(detected_roofs[roof_type]).tolist(), min_neighbors, eps)
-                elif group ==  'group_bounding':
-                    detected_roofs[roof_type] = self.evaluation.get_bounding_rects(img_name=img_name, rows=1200, 
-                                cols=2000, detections=detected_roofs[roof_type]) 
-                else:
-                    pass
+
+                #GROUPING
+                with Timer() as t:
+                    if self.group == 'group_rectangles':
+                        raise ValueError('We need to consider the angles here also')
+                        detected_roofs[roof_type], weights = cv2.groupRectangles(np.array(detected_roofs[roof_type]).tolist(), min_neighbors, eps)
+                    elif self.group ==  'group_bounding':
+                        raise ValueError('We need to consider the angles here also')
+                        detected_roofs[roof_type] = self.evaluation.get_bounding_rects(img_name=img_name, rows=1200, 
+                                    cols=2000, detections=detected_roofs[roof_type]) 
+                    else:
+                        pass
+                self.viola_detections.total_time += t.secs
+                
             self.viola_detections.set_detections(img_name=img_name, detection_list=detected_roofs)
-            return detected_roofs, img
+            return detected_roofs, img, rotated_images
+
+
+    def mark_save_current_rotation(self, img_name, img, detections, angle):
+        img = self.evaluation.mark_roofs_on_img(img_name=img_name, img=img, roofs=detections, color=(0,0,255))
+        cv2.imwrite('{0}_angle{1}.jpg'.format(self.out_folder+img_name[:-4], angle), img)
+        
+
+
+    def transform_points(self, detections, img, theta):
+        print 'transforming points...'
+        final_detections = list()
+        for x,y,w,h in detections:
+            y, x = (utils.rotate_point((y,x), img, theta)) 
+            final_detections.append((x,y,w,h))
+        return final_detections
+
+
 
 